2a1a13a7c3a51a5b15e2abf1e2fc721076776519
liang799 <71926348+liang799@users.noreply.github.com>
2022-09-08 10:26:16 +0800

    Implemented callback hell without timeout

--- async/main.js
+++ async/main.js
@@ -1,16 +1,28 @@
 const aliceTumbling = [
-  { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
 ];
 
 const aliceTiming = {
   duration: 2000,
   iterations: 1,
-  fill: 'forwards'
-}
+  fill: "forwards",
+};
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-alice1.animate(aliceTumbling, aliceTiming);
+function spin(aliceNum, callback) {
+  const animated = aliceNum.animate(aliceTumbling, aliceTiming);
+  callback(animated);
+}
+
+/* Callback Hell */
+spin(alice1, spin(alice2, () => alice3.animate(aliceTumbling, aliceTiming)))
+
+
+/* Nesting Hell */
+
+
+
